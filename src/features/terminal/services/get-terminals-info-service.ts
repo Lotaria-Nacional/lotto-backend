38d196a546f41f0e7d7a073203474e74d6cf493e
Paulods0 <pauloluguenda0@gmail.com>
2025-09-22 17:08:06 +0100

    add broken POS on get pos infos

--- src/features/terminal/services/get-terminals-info-service.ts
+++ src/features/terminal/services/get-terminals-info-service.ts
@@ -5,7 +5,7 @@ export async function getTerminalsInfoService() {
 
   const stock = await prisma.terminal.count({ where: { status: 'stock' } });
   const on_field = await prisma.terminal.count({ where: { status: 'on_field' } });
-  const maintenance = await prisma.terminal.count({ where: { status: 'maintenance' } });
+  const maintenance = await prisma.terminal.count({ where: { status: 'broken' } });
   const fixed = await prisma.terminal.count({ where: { status: 'fixed' } });
 
   return { total, stock, on_field, maintenance, fixed };
