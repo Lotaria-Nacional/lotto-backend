ceee52924ab8f65507ff6cd8415404e682934b61
Paulods0 <pauloluguenda0@gmail.com>
2025-09-26 15:32:48 +0100

    fix: admin fetch service

--- src/features/references/controllers/fetch-admins.controller.ts
+++ src/features/references/controllers/fetch-admins.controller.ts
@@ -1,7 +1,12 @@
 import type { Request, Response } from 'express';
+import { HttpStatus } from '../../../constants/http';
 import { fetchManyAdminsService } from '../services';
+import { paramsSchema } from '../../../schemas/common/query.schema';
 
-export async function fetchAdminsController(_req: Request, res: Response) {
-  const response = await fetchManyAdminsService();
-  return res.status(200).json(response);
+export async function fetchAdminsController(req: Request, res: Response) {
+  const params = paramsSchema.parse(req.query);
+
+  const response = await fetchManyAdminsService(params);
+
+  return res.status(HttpStatus.OK).json(response);
 }
--- src/features/references/services/fetch-admins-service.ts
+++ src/features/references/services/fetch-admins-service.ts
@@ -1,7 +1,21 @@
+import { Prisma } from '@prisma/client';
 import prisma from '../../../lib/prisma';
+import { PaginationParams } from '../../../@types/pagination-params';
 
-export async function fetchManyAdminsService() {
+export async function fetchManyAdminsService(params: PaginationParams) {
+  let where: Prisma.AdministrationWhereInput = {};
+
+  if (params.query) {
+    where = {
+      name: {
+        contains: params.query,
+        mode: 'insensitive',
+      },
+    };
+  }
+
   const admins = await prisma.administration.findMany({
+    where,
     include: {
       cities: true,
       licences: true,
